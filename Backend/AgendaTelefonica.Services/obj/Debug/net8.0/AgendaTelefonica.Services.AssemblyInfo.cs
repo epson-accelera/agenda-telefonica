cab0dfcb6441e2f8f36e8159c54844d030b2094f
Epson Lopes <epson.lopes@accelera.com.br>
2025-08-29 02:47:29 -0300

    feat: aplicação funcionando localmente - pronta para deploy

--- Backend/AgendaTelefonica.Services/obj/Debug/net8.0/AgendaTelefonica.Services.AssemblyInfo.cs
+++ Backend/AgendaTelefonica.Services/obj/Debug/net8.0/AgendaTelefonica.Services.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AgendaTelefonica.Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b57ee3425d2e81dd3130c71b695b8ade40f40d8")]
 [assembly: System.Reflection.AssemblyProductAttribute("AgendaTelefonica.Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AgendaTelefonica.Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
